3cbe45a2dba39c444645addceda843ef73143a1a
venkatesh.b <venkatesh.b@changepond.com>
2024-03-06 18:31:22 +0530

    fm

---
+++ dbo/Tables/tbl_users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbl_users] (
+    [Id]   INT           NOT NULL,
+    [Name] VARCHAR (255) NOT NULL,
+    [age]  INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
